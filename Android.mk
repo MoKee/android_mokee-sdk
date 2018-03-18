LineageOS/android_lineage-sdk

sdk: ilama API (9)
Change-Id: I22ec35e3ab59fe64c223e8e3433e12c05870a1c0
Signed-off-by: Joey <joey@lineageos.org>

Android.mk
@@ -215,7 +215,7 @@ lineage_platform_docs_java_libraries := \
 lineage_platform_docs_SDK_VERSION := 15.1
 
 # release version
-lineage_platform_docs_SDK_REL_ID := 8
+lineage_platform_docs_SDK_REL_ID := 9
 
 lineage_platform_docs_LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 
@@ -297,7 +297,8 @@ LOCAL_DROIDDOC_OPTIONS := \
         -since $(LINEAGE_SRC_API_DIR)/5.txt 5 \
         -since $(LINEAGE_SRC_API_DIR)/6.txt 6 \
         -since $(LINEAGE_SRC_API_DIR)/7.txt 7 \
-        -since $(LINEAGE_SRC_API_DIR)/8.txt 8
+		-since $(LINEAGE_SRC_API_DIR)/8.txt 8 \
+		-since $(LINEAGE_SRC_API_DIR)/9.txt 9
 
 $(full_target): $(lineage_framework_built) $(gen)
 include $(BUILD_DROIDDOC)

sdk/src/java/lineageos/os/Build.java
@@ -45,6 +45,7 @@ public class Build {
         sdkMap.put(LINEAGE_VERSION_CODES.FIG, "Fig");
         sdkMap.put(LINEAGE_VERSION_CODES.GUAVA, "Guava");
         sdkMap.put(LINEAGE_VERSION_CODES.HACKBERRY, "Hackberry");
+        sdkMap.put(LINEAGE_VERSION_CODES.ILAMA, "Ilama");
     }
 
     /** Various version strings. */
@@ -168,6 +169,16 @@ public class Build {
          * <p>Unused APIs have been removed.</p>
          */
         public static final int HACKBERRY = 8;
+
+        /**
+         * March 2018: The 1st iteration of the platform sdk for LineageOS
+         * <p>Applications targeting this or a later version will get access to these
+         * new features!</p>
+         * <ul>
+         * <li>Styles API to customize the system and get customized basing on the system style
+         * </ul>
+         */
+         public static final int ILAMA = 9;
     }
 
     /**

sdk/src/java/lineageos/os/Concierge.java
@@ -64,7 +64,7 @@ public final class Concierge {
      * {@link LINEAGE_VERSION_CODES}
      * @hide
      */
-    public static final int PARCELABLE_VERSION = LINEAGE_VERSION_CODES.HACKBERRY;
+    public static final int PARCELABLE_VERSION = LINEAGE_VERSION_CODES.ILAMA;
 
     /**
      * Tell the concierge to receive our parcel, so we can get information from it.

MoKee/android_mokee-sdk

Android.mk
@@ -215,7 +215,7 @@ mokee_platform_docs_java_libraries := \
 mokee_platform_docs_SDK_VERSION := 81.0
 
 # release version
-mokee_platform_docs_SDK_REL_ID := 8
+mokee_platform_docs_SDK_REL_ID := 9
 
 mokee_platform_docs_LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 
@@ -296,7 +296,8 @@ LOCAL_DROIDDOC_OPTIONS := \
         -since $(MOKEE_SRC_API_DIR)/5.txt 5 \
         -since $(MOKEE_SRC_API_DIR)/6.txt 6 \
         -since $(MOKEE_SRC_API_DIR)/7.txt 7 \
-        -since $(MOKEE_SRC_API_DIR)/8.txt 8
+        -since $(MOKEE_SRC_API_DIR)/8.txt 8 \
+        -since $(MOKEE_SRC_API_DIR)/9.txt 9
 
 $(full_target): $(mokee_framework_built) $(gen)
 include $(BUILD_DROIDDOC)

sdk/src/java/mokee/os/Build.java
@@ -39,6 +39,7 @@ public class Build {
         sdkMap.put(MK_VERSION_CODES.FIG, "Fig");
         sdkMap.put(MK_VERSION_CODES.GUAVA, "Guava");
         sdkMap.put(MK_VERSION_CODES.HACKBERRY, "Hackberry");
+        sdkMap.put(MK_VERSION_CODES.ILAMA, "Ilama");
     }
 
     /** Various version strings. */
@@ -162,6 +163,16 @@ public class Build {
          * <p>Unused APIs have been removed.</p>
          */
         public static final int HACKBERRY = 8;
+
+        /**
+         * March 2018: The 1st iteration of the platform sdk for MoKee
+         * <p>Applications targeting this or a later version will get access to these
+         * new features!</p>
+         * <ul>
+         * <li>Styles API to customize the system and get customized basing on the system style
+         * </ul>
+         */
+        public static final int ILAMA = 9;
     }
 
     /**

sdk/src/java/mokee/os/Concierge.java
@@ -65,7 +65,7 @@ public final class Concierge {
      * {@link MK_VERSION_CODES}
      * @hide
      */
-    public static final int PARCELABLE_VERSION = MK_VERSION_CODES.HACKBERRY;
+    public static final int PARCELABLE_VERSION = MK_VERSION_CODES.ILAMA;
 
     /**
      * Tell the concierge to receive our parcel, so we can get information from it.