d55c0d18d925eba84b7ffc673dcb53ca912b4a10
LuK1337 <priv.luk@gmail.com>
2018-07-30 21:36:11 +0200

    sdk: Add migration for /missing/ custom button actions
    * Since we removed onehand from settings
      we need to make sure user doesn't have them
      set in settings otherwise we will hit
      ArrayIndexOutOfBoundsException exception.
    * Fixes : BUGBASH-1525
    
    Change-Id: I7a5737ecfe5e70c3f0108c03b3e8745eade80433

--- packages/MKSettingsProvider/src/org/mokee/mksettings/MKDatabaseHelper.java
+++ packages/MKSettingsProvider/src/org/mokee/mksettings/MKDatabaseHelper.java
@@ -52,7 +52,7 @@ public class MKDatabaseHelper extends SQLiteOpenHelper{
     private static final boolean LOCAL_LOGV = false;
 
     private static final String DATABASE_NAME = "mksettings.db";
-    private static final int DATABASE_VERSION = 8;
+    private static final int DATABASE_VERSION = 9;
 
     public static class MKTableNames {
         public static final String TABLE_SYSTEM = "system";
@@ -280,6 +280,21 @@ public class MKDatabaseHelper extends SQLiteOpenHelper{
                 db.endTransaction();
             }
             upgradeVersion = 8;
+        }
+
+        if (upgradeVersion < 9) {
+            if (mUserHandle == UserHandle.USER_OWNER) {
+                db.execSQL("UPDATE system SET value = '0' WHERE value IN ('10', '11') AND name IN ("
+                        + "'" + MKSettings.System.KEY_HOME_LONG_PRESS_ACTION + "',"
+                        + "'" + MKSettings.System.KEY_HOME_DOUBLE_TAP_ACTION + "',"
+                        + "'" + MKSettings.System.KEY_MENU_ACTION + "',"
+                        + "'" + MKSettings.System.KEY_MENU_LONG_PRESS_ACTION + "',"
+                        + "'" + MKSettings.System.KEY_ASSIST_ACTION + "',"
+                        + "'" + MKSettings.System.KEY_ASSIST_LONG_PRESS_ACTION + "',"
+                        + "'" + MKSettings.System.KEY_APP_SWITCH_ACTION + "',"
+                        + "'" + MKSettings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION + "')");
+            }
+            upgradeVersion = 9;
         }
         // *** Remember to update DATABASE_VERSION above!
 
