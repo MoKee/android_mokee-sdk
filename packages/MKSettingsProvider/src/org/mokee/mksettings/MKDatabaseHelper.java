e3463003dfb35900627b5cd1cd34516d2c669da0
Han Wang <416810799@qq.com>
2018-12-23 18:45:43 +0100

    mokee-sdk: Remove useless logic on database upgrading
     * The upgradeVersion < newVersion part is never reachable because we
       increase upgradeVersion unconditionally. Make it back to stage by
       nuking it as no one has experience issues before.
    
    Change-Id: Icac1e18c192292ad3147fd4af4c862e93ef34ba5

--- packages/MKSettingsProvider/src/org/mokee/mksettings/MKDatabaseHelper.java
+++ packages/MKSettingsProvider/src/org/mokee/mksettings/MKDatabaseHelper.java
@@ -166,13 +166,11 @@ public class MKDatabaseHelper extends SQLiteOpenHelper{
             db.beginTransaction();
             try {
                 loadSettings(db);
-
                 db.setTransactionSuccessful();
-
-                upgradeVersion = 2;
             } finally {
                 db.endTransaction();
             }
+            upgradeVersion = 2;
         }
 
         if (upgradeVersion < 3) {
@@ -325,21 +323,6 @@ public class MKDatabaseHelper extends SQLiteOpenHelper{
             upgradeVersion = 10;
         }
         // *** Remember to update DATABASE_VERSION above!
-
-        if (upgradeVersion < newVersion) {
-            Log.w(TAG, "Got stuck trying to upgrade db. Old version: " + oldVersion
-                    + ", version stuck at: " +  upgradeVersion + ", new version: "
-                            + newVersion + ". Must wipe the mk settings provider.");
-
-            dropDbTable(db, MKTableNames.TABLE_SYSTEM);
-            dropDbTable(db, MKTableNames.TABLE_SECURE);
-
-            if (mUserHandle == UserHandle.USER_OWNER) {
-                dropDbTable(db, MKTableNames.TABLE_GLOBAL);
-            }
-
-            onCreate(db);
-        }
     }
 
     private void moveSettingsToNewTable(SQLiteDatabase db,
