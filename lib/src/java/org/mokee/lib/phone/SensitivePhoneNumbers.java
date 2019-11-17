76ed7b8c8584b04d96933b9023e2ceb5186f8780
Michael W <baddaemon87@gmail.com>
2019-11-17 15:15:11 +0100

    sdk: Make phone a new compile target
    * Moving SensitivePhoneNumbers to sdk got us significantly bigger
      zip packages due to libphonenumber being part of every package that is
      using org.mokee.platform or org.mokee.platform.internal
    * In order to correct this, move the "phone" folder to an own library that
      statically links libphonennumber and subsequentially make use of that
      package where needed
    
    Change-Id: I4effd7a9248290aeb23ab47ff8e30385dce4401a

--- lib/src/java/org/mokee/lib/phone/SensitivePhoneNumbers.java
+++ lib/src/java/org/mokee/lib/phone/SensitivePhoneNumbers.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
  * Copyright (C) 2017-2019 The LineageOS Project
+ * Copyright (C) 2017-2019 The MoKee Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.mokee.internal.phone;
+package org.mokee.lib.phone;
 
 import android.content.Context;
 import android.os.Environment;
