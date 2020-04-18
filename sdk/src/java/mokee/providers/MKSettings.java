5b64955b3f10d82d5772b2e2d78bd4d72897eb36
rautamak <timi.rautamaki@gmail.com>
2020-04-18 16:29:22 +0300

    sdk: Allow translucent notifications background on lockscreen [2/3]
    Change-Id: I5f2ee373e446713aee236540f8d16e2685227b32

--- sdk/src/java/mokee/providers/MKSettings.java
+++ sdk/src/java/mokee/providers/MKSettings.java
@@ -2982,6 +2982,13 @@ public final class MKSettings {
         public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
+         * Whether to have translucent background on lockscreen notifications
+         * @hide
+         */
+        public static final String LOCKSCREEN_TRANSLUCENT_NOTIFICATIONS_BG_ENABLED
+                = "lockscreen_translucent_notifications_bg_enabled";
+
+        /**
          * Whether to activate double tap to sleep on keyguard
          * Boolean setting. 0 = off, 1 = on.
          * @hide
