43a97c005f514583d0886a21da1e798397773715
Jesse Chan <jc@lineageos.org>
2020-05-17 22:43:37 +0800

    Implement click to take partial screenshot [3/3]
    Change-Id: I9de0cd4f1b793119a7dd45a95b040f70d6c151a6
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- sdk/src/java/mokee/providers/MKSettings.java
+++ sdk/src/java/mokee/providers/MKSettings.java
@@ -2131,6 +2131,16 @@ public final class MKSettings {
                 sBooleanValidator;
 
         /**
+         * Whether to take partial screenshot with volume down + power click.
+         * 0 = 0ff, 1 = on
+         */
+        public static final String CLICK_PARTIAL_SCREENSHOT = "click_partial_screenshot";
+
+        /** @hide */
+        public static final Validator CLICK_PARTIAL_SCREENSHOT_VALIDATOR =
+                sBooleanValidator;
+
+        /**
          * Screen off fod
          * @hide
          */
@@ -2404,6 +2414,8 @@ public final class MKSettings {
                     LONG_SCREEN_APPS_VALIDATOR);
             VALIDATORS.put(FORCE_SHOW_NAVBAR,
                     FORCE_SHOW_NAVBAR_VALIDATOR);
+            VALIDATORS.put(CLICK_PARTIAL_SCREENSHOT,
+                    CLICK_PARTIAL_SCREENSHOT_VALIDATOR);
             VALIDATORS.put(SWIPE_TO_SCREENSHOT, SWIPE_TO_SCREENSHOT_VALIDATOR);
             VALIDATORS.put(FOD_GESTURE, FOD_GESTURE_VALIDATOR);
         };
