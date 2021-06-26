LineageOS/android_lineage-sdk

Revert "sdk: Deprecate POWER_MENU_ACTIONS"
This reverts commit d8742f0727aad8187a903a0afbafbc909edf1d1c.

Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
Change-Id: I4071297652a6cd0a47d6c642909b32f742e60781

sdk/src/java/lineageos/content/Intent.java
@@ -104,6 +104,17 @@ public class Intent {
             "lineageos.intent.extra.LID_STATE";
 
     /**
+     * Broadcast Action: Update preferences for the power menu dialog. This is to provide a
+     * way for the preferences that need to be enabled/disabled to update because they were
+     * toggled elsewhere in the settings (ie screenshot, user switcher, etc) so we don't have
+     * to do constant lookups while we wait for the menu to be created. Getting the values once
+     * when necessary is enough.
+     *@hide
+     */
+    public static final String ACTION_UPDATE_POWER_MENU =
+            "lineageos.intent.action.UPDATE_POWER_MENU";
+
+    /**
      * Broadcast action: notify SystemUI that LiveDisplay service has finished initialization.
      * @hide
      */

sdk/src/java/org/lineageos/internal/util/PowerMenuConstants.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2017-2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.internal.util;
+
+/* Master list of all actions for the power menu */
+public class PowerMenuConstants {
+    public static final String GLOBAL_ACTION_KEY_POWER = "power";
+    public static final String GLOBAL_ACTION_KEY_RESTART = "restart";
+    public static final String GLOBAL_ACTION_KEY_SCREENSHOT = "screenshot";
+    public static final String GLOBAL_ACTION_KEY_AIRPLANE = "airplane";
+    public static final String GLOBAL_ACTION_KEY_USERS = "users";
+    public static final String GLOBAL_ACTION_KEY_SETTINGS = "settings";
+    public static final String GLOBAL_ACTION_KEY_LOCKDOWN = "lockdown";
+    public static final String GLOBAL_ACTION_KEY_BUGREPORT = "bugreport";
+    public static final String GLOBAL_ACTION_KEY_SILENT = "silent";
+    public static final String GLOBAL_ACTION_KEY_VOICEASSIST = "voiceassist";
+    public static final String GLOBAL_ACTION_KEY_ASSIST = "assist";
+    public static final String GLOBAL_ACTION_KEY_LOGOUT = "logout";
+    public static final String GLOBAL_ACTION_KEY_EMERGENCY = "emergency";
+
+    /**
+     * Advanced restart menu actions
+     */
+    public static final String GLOBAL_ACTION_KEY_RESTART_RECOVERY = "restart_recovery";
+    public static final String GLOBAL_ACTION_KEY_RESTART_BOOTLOADER = "restart_bootloader";
+    public static final String GLOBAL_ACTION_KEY_RESTART_DOWNLOAD = "restart_download";
+    public static final String GLOBAL_ACTION_KEY_RESTART_FASTBOOT = "restart_fastboot";
+
+    private static String[] ALL_ACTIONS = {
+        GLOBAL_ACTION_KEY_POWER,
+        GLOBAL_ACTION_KEY_RESTART,
+        GLOBAL_ACTION_KEY_SCREENSHOT,
+        GLOBAL_ACTION_KEY_AIRPLANE,
+        GLOBAL_ACTION_KEY_USERS,
+        GLOBAL_ACTION_KEY_SETTINGS,
+        GLOBAL_ACTION_KEY_LOCKDOWN,
+        GLOBAL_ACTION_KEY_BUGREPORT,
+        GLOBAL_ACTION_KEY_SILENT,
+        GLOBAL_ACTION_KEY_VOICEASSIST,
+        GLOBAL_ACTION_KEY_ASSIST,
+        GLOBAL_ACTION_KEY_LOGOUT,
+        GLOBAL_ACTION_KEY_EMERGENCY,
+    };
+
+    public static String[] getAllActions() {
+        return ALL_ACTIONS;
+    }
+}

MoKee/android_mokee-sdk

sdk/src/java/mokee/content/Intent.java
@@ -105,6 +105,17 @@ public class Intent {
             "mokee.intent.extra.LID_STATE";
 
     /**
+     * Broadcast Action: Update preferences for the power menu dialog. This is to provide a
+     * way for the preferences that need to be enabled/disabled to update because they were
+     * toggled elsewhere in the settings (ie screenshot, user switcher, etc) so we don't have
+     * to do constant lookups while we wait for the menu to be created. Getting the values once
+     * when necessary is enough.
+     *@hide
+     */
+    public static final String ACTION_UPDATE_POWER_MENU =
+            "mokee.intent.action.UPDATE_POWER_MENU";
+
+    /**
      * Broadcast action: notify SystemUI that LiveDisplay service has finished initialization.
      * @hide
      */

sdk/src/java/org/mokee/internal/util/PowerMenuConstants.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2017-2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mokee.internal.util;
+
+/* Master list of all actions for the power menu */
+public class PowerMenuConstants {
+    public static final String GLOBAL_ACTION_KEY_POWER = "power";
+    public static final String GLOBAL_ACTION_KEY_RESTART = "restart";
+    public static final String GLOBAL_ACTION_KEY_SCREENSHOT = "screenshot";
+    public static final String GLOBAL_ACTION_KEY_AIRPLANE = "airplane";
+    public static final String GLOBAL_ACTION_KEY_USERS = "users";
+    public static final String GLOBAL_ACTION_KEY_SETTINGS = "settings";
+    public static final String GLOBAL_ACTION_KEY_LOCKDOWN = "lockdown";
+    public static final String GLOBAL_ACTION_KEY_BUGREPORT = "bugreport";
+    public static final String GLOBAL_ACTION_KEY_SILENT = "silent";
+    public static final String GLOBAL_ACTION_KEY_VOICEASSIST = "voiceassist";
+    public static final String GLOBAL_ACTION_KEY_ASSIST = "assist";
+    public static final String GLOBAL_ACTION_KEY_LOGOUT = "logout";
+    public static final String GLOBAL_ACTION_KEY_EMERGENCY = "emergency";
+
+    /**
+     * Advanced restart menu actions
+     */
+    public static final String GLOBAL_ACTION_KEY_RESTART_RECOVERY = "restart_recovery";
+    public static final String GLOBAL_ACTION_KEY_RESTART_BOOTLOADER = "restart_bootloader";
+    public static final String GLOBAL_ACTION_KEY_RESTART_DOWNLOAD = "restart_download";
+    public static final String GLOBAL_ACTION_KEY_RESTART_FASTBOOT = "restart_fastboot";
+
+    private static String[] ALL_ACTIONS = {
+        GLOBAL_ACTION_KEY_POWER,
+        GLOBAL_ACTION_KEY_RESTART,
+        GLOBAL_ACTION_KEY_SCREENSHOT,
+        GLOBAL_ACTION_KEY_AIRPLANE,
+        GLOBAL_ACTION_KEY_USERS,
+        GLOBAL_ACTION_KEY_SETTINGS,
+        GLOBAL_ACTION_KEY_LOCKDOWN,
+        GLOBAL_ACTION_KEY_BUGREPORT,
+        GLOBAL_ACTION_KEY_SILENT,
+        GLOBAL_ACTION_KEY_VOICEASSIST,
+        GLOBAL_ACTION_KEY_ASSIST,
+        GLOBAL_ACTION_KEY_LOGOUT,
+        GLOBAL_ACTION_KEY_EMERGENCY,
+    };
+
+    public static String[] getAllActions() {
+        return ALL_ACTIONS;
+    }
+}
8cdcadc74d071374e44565e50d787b88735579e1
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-03-29 21:01:34 +0400

    Revert "sdk: Deprecate POWER_MENU_ACTIONS"
    This reverts commit d8742f0727aad8187a903a0afbafbc909edf1d1c.
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I4071297652a6cd0a47d6c642909b32f742e60781

--- sdk/src/java/mokee/providers/MoKeeSettings.java
+++ sdk/src/java/mokee/providers/MoKeeSettings.java
@@ -2902,10 +2902,8 @@ public final class MoKeeSettings {
 
         /**
          * String to contain power menu actions
-         * @deprecated
          * @hide
          */
-        @Deprecated
         public static final String POWER_MENU_ACTIONS = "power_menu_actions";
 
         /**
